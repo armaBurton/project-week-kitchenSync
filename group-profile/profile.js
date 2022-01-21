c9943cdbba9cf09b97c1fd381db38ebe4f98df1d
James Keicher <jamesdkeicher@gmail.com>
2022-01-20 20:12:19 -0800

    hooked up features in about page

--- group-profile/profile.js
+++ group-profile/profile.js
@@ -1,3 +1,94 @@
+import { logout, redirectIfLoggedIn, signInUser, signupUser } from '../script/fetch-utils.js';
 import { renderHeader } from '../script/render-utils.js';
 
+const jack = document.getElementById('jack');
+const philippe = document.getElementById('philippe');
+const arma = document.getElementById('arma');
+const alex = document.getElementById('alex');
+
+const logoutButton = document.querySelector('#logout-button');
+
+const signUpButton = document.querySelector('#sign-up-button');
+const signUpDisplay = document.getElementById('signup');
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+
+const signInButton = document.querySelector('#sign-in-button');
+const signInDisplay = document.getElementById('login');
+const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
+
+const xOutButton = document.querySelectorAll('.close-popup');
+const switchModalButton = document.querySelector('.new-user-span');
+
+
+
+
 renderHeader();
+
+logoutButton.addEventListener('click', async() => {
+    await logout();
+    location.replace('../');
+});
+
+signUpButton.addEventListener('click', () => {
+    signUpDisplay.classList.remove('visibility');
+});
+
+signInButton.addEventListener('click', () => {
+    signInDisplay.classList.remove('visibility');
+
+});
+
+signUpForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const data = new FormData(signUpForm);
+    const username = data.get('user-name');
+    const user = await signupUser(signUpEmail.value, signUpPassword.value, username);
+    if (user){
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const user = await signInUser(signInEmail.value, signInPassword.value);
+    if (user){
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+switchModalButton.addEventListener('click', () => {
+    signInDisplay.classList.add('visibility');
+    signUpDisplay.classList.remove('visibility');
+});
+
+for (let button of xOutButton) {
+    button.addEventListener('click', () => {
+        signInDisplay.classList.add('visibility');
+        signUpDisplay.classList.add('visibility');
+    });
+}
+
+
+alex.addEventListener('click', () => {
+    location.replace('https://github.com/alex-i-blair');
+});
+
+arma.addEventListener('click', () => {
+    location.replace('https://github.com/armaBurton');
+});
+
+philippe.addEventListener('click', () => {
+    location.replace('https://github.com/philngom');
+});
+
+jack.addEventListener('click', () => {
+    location.replace('https://github.com/CadillacJack42');
+});
